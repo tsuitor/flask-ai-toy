b17314627808c1ea91530b47bffb469d9c728a05
Terry Suitor <terry@t10r.dev>
2023-05-06 16:12:27 -0400

    initial push

---
+++ flask toy.py
@@ -0,0 +1,50 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def home():
+     return """<html>
+         <head><title>Home</title></head>
+          <body style="background:#FDFEFE;">
+            <h1 align='center'>Welcome Home!</h1>
+             </br>
+              <div class="container">
+                <form action="/login" method="POST">
+                  Username:<input type ="text", placeholder="Username">
+
+                   Password:<input type ="password", placeholder="Password">
+
+
+                    <button type="submit">Login</button>
+                 </form>
+              </div>
+        </body>
+     </html>"""
+
+
+
+
+
+@app.route("/login")
+def login():
+     username = request.args["username"]
+     password = request.args["password"]
+
+     if username != "" && password !="" :
+         return "<h1>" + str("Logged in successfully!") + "</h1>"
+     elif username == "":
+         return "<h1>" + str("Please enter valid credentials.") + "</h1>"
+     elif password == "":
+         return "<h1>" + str("Please enter valid credentials.") + "</h1>"
+     else:
+         return "<h1>" + str("Invalid Credentials!!") + "</h1>"
+
+
+
+
+
+
+if __name__ == "__main__":
+	app.run()
